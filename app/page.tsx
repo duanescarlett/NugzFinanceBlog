02ddd64f97f5ea764ef5caba572b4b18a9da761f
Duane Scarlett <duanescarlett@gmail.com>
2023-01-09 12:44:01 -0500

    Init Commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className='text-4xl'>Welcome to Nugz Finance</h1>  
+      <p>Some Content</p>
+    </div>
+  )
+}
+
+export default HomePage

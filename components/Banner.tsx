2dab142458ddd4c9cac71e5f71f5558d4190caca
Duane Scarlett <duanescarlett@gmail.com>
2023-01-23 16:09:22 -0500

    corrected font

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ const Banner = () => {
   return (
     <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
       <div>
-        <h1 className="text-7xl">Nugz Finance Blog</h1>
+        <h1 className="">Nugz Finance Blog</h1>
         <h2 className="mt-5 md:mt-0">
           <span className="underline decoration-4 decoration-[#F7AB0A]">
             Finance, Lifestyle and Self Development
